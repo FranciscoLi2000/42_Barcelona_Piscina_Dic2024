8524504f2e884fd63a45fcabf57adb492865a43a
FranciscoLi2000 <liyufeng577@gmail.com>
2025-08-10 15:31:29 +0000

    C03

--- C03/ft_strlcat.c
+++ C03/ft_strlcat.c
@@ -1,8 +1,19 @@
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	dest_len;
 	unsigned int	src_len;
+	unsigned int	i;
 
 	src_len = ft_strlen(src);
 	dest_len = ft_strlen(dest);
@@ -11,9 +22,9 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	i = 0;
 	while (src[i] && (dest_len + i < size - 1))
 	{
-		dst[dest_len + i] = src[i];
+		dest[dest_len + i] = src[i];
 		i++;
 	}
-	dst[dest_len + i] = '\0';
+	dest[dest_len + i] = '\0';
 	return (dest_len + src_len);
 }
--- C03/ft_strncmp.c
+++ C03/ft_strncmp.c
@@ -3,8 +3,10 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
-	while ((s1[i] || s2[i]) && i < n)
+	while (s1[i] && s2[i] && i < n)
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
